13771026e4a525c7274e81e3ff9e3cfc3b7c9f44
Olek <roguezilla0@gmail.com>
2020-11-25 19:53:54 +0000

    spaces to tabs

--- instagram.py
+++ instagram.py
@@ -10,132 +10,132 @@ import perms
 
 """
 'msgid': {
-    'size': 2,
-    'curr': 1,
-    '1': 'url',
-    '2': 'url'
+	'size': 2,
+	'curr': 1,
+	'1': 'url',
+	'2': 'url'
 }
 """
 gallery_cache = dict()
 
 def populate_cache(data, msg: discord.Message, repopulate=False):
-    if 'edge_sidecar_to_children' not in data:
-        # if data doesn't have media_metadata, then it's not a gallery
-        return 0
+	if 'edge_sidecar_to_children' not in data:
+		# if data doesn't have media_metadata, then it's not a gallery
+		return 0
 
-    gallery_cache[str(msg.channel.id) + str(msg.id)] = {
-        'size': len(data['edge_sidecar_to_children']['edges']),
-        'curr': 1
-    }
+	gallery_cache[str(msg.channel.id) + str(msg.id)] = {
+		'size': len(data['edge_sidecar_to_children']['edges']),
+		'curr': 1
+	}
 
-    for i in range(len(data['edge_sidecar_to_children']['edges'])):
-        gallery_cache[str(msg.channel.id) + str(msg.id)][i + 1] = data['edge_sidecar_to_children']['edges'][i]['node']['display_url']
+	for i in range(len(data['edge_sidecar_to_children']['edges'])):
+		gallery_cache[str(msg.channel.id) + str(msg.id)][i + 1] = data['edge_sidecar_to_children']['edges'][i]['node']['display_url']
 
-    # when repopulating, we need to match the current picture with its index
-    if repopulate:
-        url = msg.embeds[0].image.__getattribute__('url')
-        for key in gallery_cache[str(msg.channel.id) + str(msg.id)]:
-            if gallery_cache[str(msg.channel.id) + str(msg.id)][key] == url:
-                gallery_cache[str(msg.channel.id) + str(msg.id)]['curr'] = key
+	# when repopulating, we need to match the current picture with its index
+	if repopulate:
+		url = msg.embeds[0].image.__getattribute__('url')
+		for key in gallery_cache[str(msg.channel.id) + str(msg.id)]:
+			if gallery_cache[str(msg.channel.id) + str(msg.id)][key] == url:
+				gallery_cache[str(msg.channel.id) + str(msg.id)]['curr'] = key
 
 class Instagram(commands.Cog):
-    def __init__(self, bot, db):
-        self.bot: commands.Bot = bot
-        self.db = db
+	def __init__(self, bot, db):
+		self.bot: commands.Bot = bot
+		self.db = db
 
-    @staticmethod
-    def url_data(url):
-        # cut out useless stuff and form an api url
-        url = url.split("?")[0]
-        api_url = url + '?__a=1'
-        return requests.get(api_url, headers = {'User-agent': 'RogueStarboard v1.0'}).json()['graphql']['shortcode_media']
+	@staticmethod
+	def url_data(url):
+		# cut out useless stuff and form an api url
+		url = url.split("?")[0]
+		api_url = url + '?__a=1'
+		return requests.get(api_url, headers = {'User-agent': 'RogueStarboard v1.0'}).json()['graphql']['shortcode_media']
 
-    @staticmethod
-    def return_link(url, msg=None):
-        data = Instagram.url_data(url)
-        # only galeries have media_metadata
-        if 'edge_sidecar_to_children' in data:
-            # thankfully edge_sidecar_to_children has the images in the right order
-            ret = data['edge_sidecar_to_children']['edges'][0]['node']['display_url']
-            # as the link is a gallery, we need to populate the gallery cache
-            if msg: populate_cache(data, msg)
-        else:
-            ret = data['display_url']
-        return ret
+	@staticmethod
+	def return_link(url, msg=None):
+		data = Instagram.url_data(url)
+		# only galeries have media_metadata
+		if 'edge_sidecar_to_children' in data:
+			# thankfully edge_sidecar_to_children has the images in the right order
+			ret = data['edge_sidecar_to_children']['edges'][0]['node']['display_url']
+			# as the link is a gallery, we need to populate the gallery cache
+			if msg: populate_cache(data, msg)
+		else:
+			ret = data['display_url']
+		return ret
 
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        # NoneType exceptions without this, why? only god knows
-        if message.guild is None:
-            return
+	@commands.Cog.listener()
+	async def on_message(self, message: discord.Message):
+		# NoneType exceptions without this, why? only god knows
+		if message.guild is None:
+			return
 
-        if self.db[str(message.guild.id)].find_one(name='instagram_embed')['value'] == '1':
-            url = re.findall(r'(<?(https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*>?)', message.content)
-            if url and 'instagram.com/p/' in url[0][0] and (url[0][0][0] != '<' and url[0][0][-1] != '>'):
-                ret = self.return_link(url[0][0], msg=message)
-                if ret:
-                    embed=discord.Embed(title='Instagram embed', description=message.content)
-                    embed.set_image(url=ret)
-                    embed.add_field(name='Sender', value=message.author.mention)
-                    sent: discord.Message = await message.channel.send(embed=embed)
+		if self.db[str(message.guild.id)].find_one(name='instagram_embed')['value'] == '1':
+			url = re.findall(r'(<?(https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*>?)', message.content)
+			if url and 'instagram.com/p/' in url[0][0] and (url[0][0][0] != '<' and url[0][0][-1] != '>'):
+				ret = self.return_link(url[0][0], msg=message)
+				if ret:
+					embed=discord.Embed(title='Instagram embed', description=message.content)
+					embed.set_image(url=ret)
+					embed.add_field(name='Sender', value=message.author.mention)
+					sent: discord.Message = await message.channel.send(embed=embed)
 
-                    if str(message.channel.id) + str(message.id) in gallery_cache:
-                        # copy old message info into the new message(our embed) and delete old message from the dictionary
-                        gallery_cache[str(sent.channel.id) + str(sent.id)] = gallery_cache[str(message.channel.id) + str(message.id)]
-                        del gallery_cache[str(message.channel.id) + str(message.id)]
+					if str(message.channel.id) + str(message.id) in gallery_cache:
+						# copy old message info into the new message(our embed) and delete old message from the dictionary
+						gallery_cache[str(sent.channel.id) + str(sent.id)] = gallery_cache[str(message.channel.id) + str(message.id)]
+						del gallery_cache[str(message.channel.id) + str(message.id)]
 
-                        embed: discord.Embed = sent.embeds[0]
-                        embed.add_field(name='Page', value=f"{gallery_cache[str(sent.channel.id) + str(sent.id)]['curr']}/{gallery_cache[str(sent.channel.id) + str(sent.id)]['size']}", inline=True)
-                        await sent.edit(embed=embed)
+						embed: discord.Embed = sent.embeds[0]
+						embed.add_field(name='Page', value=f"{gallery_cache[str(sent.channel.id) + str(sent.id)]['curr']}/{gallery_cache[str(sent.channel.id) + str(sent.id)]['size']}", inline=True)
+						await sent.edit(embed=embed)
 
-                        await sent.add_reaction('⬅️')
-                        await sent.add_reaction('➡️')
+						await sent.add_reaction('⬅️')
+						await sent.add_reaction('➡️')
 
-                    # we don't really the message and it only occupies space now
-                    await message.delete()
+					# we don't really the message and it only occupies space now
+					await message.delete()
 
-    @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        msg_id = str(payload.channel_id)+str(payload.message_id)
-        msg: discord.Message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+	@commands.Cog.listener()
+	async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+		msg_id = str(payload.channel_id)+str(payload.message_id)
+		msg: discord.Message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
 
-        # return if the reaction was from a bot or there are no embeds or the message that was reacted to wasn't from the bot
-        if payload.member.bot or not msg.embeds or msg.author.id != self.bot.user.id or msg.embeds[0].title != 'Instagram embed':
-            return
+		# return if the reaction was from a bot or there are no embeds or the message that was reacted to wasn't from the bot
+		if payload.member.bot or not msg.embeds or msg.author.id != self.bot.user.id or msg.embeds[0].title != 'Instagram embed':
+			return
 
-        # we want to repopulate the cache when the bot is restarted
-        if msg_id not in gallery_cache:
-            url = re.findall(r'((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', msg.embeds[0].description)
-            # see populate_cache
-            if populate_cache(Instagram.url_data(url[0][0]), msg, True) == 0:
-                return
+		# we want to repopulate the cache when the bot is restarted
+		if msg_id not in gallery_cache:
+			url = re.findall(r'((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', msg.embeds[0].description)
+			# see populate_cache
+			if populate_cache(Instagram.url_data(url[0][0]), msg, True) == 0:
+				return
 
-        if msg_id in gallery_cache:
-            if str(payload.emoji) == '➡️' or str(payload.emoji) == '⬅️':
-                embed: discord.Embed = msg.embeds[0]
-                
-                gal_size = gallery_cache[msg_id]['size']
-                curr_idx = gallery_cache[msg_id]['curr']
-            
-                if str(payload.emoji) == '➡️':
-                    curr_idx = curr_idx + 1 if curr_idx + 1 <= gal_size else 1
-                elif str(payload.emoji) == '⬅️':
-                    curr_idx = curr_idx - 1 if curr_idx - 1 >= 1 else gal_size
+		if msg_id in gallery_cache:
+			if str(payload.emoji) == '➡️' or str(payload.emoji) == '⬅️':
+				embed: discord.Embed = msg.embeds[0]
+				
+				gal_size = gallery_cache[msg_id]['size']
+				curr_idx = gallery_cache[msg_id]['curr']
+			
+				if str(payload.emoji) == '➡️':
+					curr_idx = curr_idx + 1 if curr_idx + 1 <= gal_size else 1
+				elif str(payload.emoji) == '⬅️':
+					curr_idx = curr_idx - 1 if curr_idx - 1 >= 1 else gal_size
 
-                gallery_cache[msg_id]['curr'] = curr_idx
-                new_url = gallery_cache[msg_id][curr_idx]
+				gallery_cache[msg_id]['curr'] = curr_idx
+				new_url = gallery_cache[msg_id][curr_idx]
 
-                embed.set_image(url=new_url)
-                embed.set_field_at(1, name='Page', value=f"{gallery_cache[str(msg.channel.id) + str(msg.id)]['curr']}/{gallery_cache[str(msg.channel.id) + str(msg.id)]['size']}")
+				embed.set_image(url=new_url)
+				embed.set_field_at(1, name='Page', value=f"{gallery_cache[str(msg.channel.id) + str(msg.id)]['curr']}/{gallery_cache[str(msg.channel.id) + str(msg.id)]['size']}")
 
-                await msg.edit(embed=embed)
-                await msg.remove_reaction(payload.emoji, payload.member)
+				await msg.edit(embed=embed)
+				await msg.remove_reaction(payload.emoji, payload.member)
 
-    @commands.command(brief='Toggle automatic Instagram embeds.')
-    @perms.mod()
-    async def embed_insta(self, ctx: commands.Context):
-        prev = self.db[str(ctx.guild.id)].find_one(name='instagram_embed')['value']
-        new_val = '0' if prev == '1' else '1'
-        self.db[str(ctx.guild.id)].update(dict(name='instagram_embed', value=new_val), ['name'])
+	@commands.command(brief='Toggle automatic Instagram embeds.')
+	@perms.mod()
+	async def embed_insta(self, ctx: commands.Context):
+		prev = self.db[str(ctx.guild.id)].find_one(name='instagram_embed')['value']
+		new_val = '0' if prev == '1' else '1'
+		self.db[str(ctx.guild.id)].update(dict(name='instagram_embed', value=new_val), ['name'])
 
-        await self.bot.get_user(ctx.message.author.id).send(f"instagram embeds: {'on' if new_val == '1' else 'off'}")
+		await self.bot.get_user(ctx.message.author.id).send(f"instagram embeds: {'on' if new_val == '1' else 'off'}")
