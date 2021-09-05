52953ac0ff62df5510387271e812e76e6d7bae07
Olek <roguezilla0@gmail.com>
2021-09-05 10:39:53 +0100

    remove pointless int(...)

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ async def build_info(msg: discord.Message):
 							'image',
 							'\n'.join(content[1:]) if len(content) > 1 else '',
 							msg.embeds[0].image.__getattribute__('url'),
-							await bot.fetch_user(int(msg.embeds[0].fields[0].__dict__['value'][3:len(msg.embeds[0].fields[0].__dict__['value'])-1]))
+							await bot.fetch_user(msg.embeds[0].fields[0].__dict__['value'][3:len(msg.embeds[0].fields[0].__dict__['value'])-1])
 						)
 				else:
 					set_info(
