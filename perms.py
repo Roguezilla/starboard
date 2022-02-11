e43f82887ced283133b762e3368dc87014fde465
Olek <roguezilla0@gmail.com>
2022-02-11 17:36:30 +0000

    oops

--- perms.py
+++ perms.py
@@ -4,4 +4,4 @@ async def is_owner(self: DiscPy, msg: Message):
     return await self.is_owner(msg.author.id)
 
 async def is_mod(self: DiscPy, msg: Message):
-    return await self.has_permissions(msg, self.Permissions.MANAGE_MESSAGES) or self.is_owner(msg.author.id)
+    return await self.has_permissions(msg, self.Permissions.MANAGE_MESSAGES) or await self.is_owner(msg.author.id)
