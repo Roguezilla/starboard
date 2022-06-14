b0894983c4b0889995f923325ef06c7027772f8f
Roguezilla <roguezilla0@gmail.com>
2022-06-14 17:30:50 +0100

    forgot to update the pixiv regex

--- cogs/pixiv.py
+++ cogs/pixiv.py
@@ -11,6 +11,6 @@ class Pixiv(DiscPy.Cog):
 			if event.author.bot or not db['server'].find_one(server_id = event.guild_id):
 				return
 
-			if id := re.findall(r'https:\/\/www\.pixiv\.net\/(?:en\/)?artworks\/(\d+)', event.content):
+			if id := re.findall(r'https:\/\/(?:www\.)?pixiv\.net\/(?:en\/)?artworks\/(\d+)', event.content):
 				await ctx.send_message(event.channel_id, f'https://pixiv.kmn5.li/{id[0]}?u={event.author.id}')
 				await ctx.delete_message(event)
